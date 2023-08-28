5d32b92afdd92fcbd79b32b77af34cc63b159032
Kevin Zhang <kazp058@gmail.com>
2023-08-28 00:41:15 -0500

    feat: :zap: performance result

--- launch.py
+++ launch.py
@@ -1,9 +1,9 @@
 from models import *
 
-M = 70
-N = 1000000000
+M = 60
+N = 100
 
-def greedy_students(grade, tasks):
+def greedy_students2(grade, tasks):
     selected_tasks = []
     remaining_grade = grade
 
@@ -25,6 +25,22 @@ def greedy_students(grade, tasks):
         selected_tasks.append(task)
     return selected_tasks
 
+def greedy_students(grade, tasks):
+    selected_tasks = []
+    remaining_grade = grade
+    while remaining_grade > 0 and len(tasks) > 0:
+        prefered_idtask = None
+        for __idtask in range(len(tasks)):
+            task = tasks[__idtask]
+            if ((prefered_idtask == None) 
+                or (tasks[prefered_idtask].effort >= task.effort and tasks[prefered_idtask].score <= task.score)):
+                prefered_idtask = __idtask
+        task = tasks.pop(prefered_idtask)
+        selected_tasks.append(task)
+        remaining_grade -= task.score
+
+    return selected_tasks
+
 # Diseñe un algoritmo que seleccione las tareas
 # que le otorgan una nota mayor o igual a la 
 # minima que requieren el menor esfuerzo del
@@ -32,11 +48,22 @@ def greedy_students(grade, tasks):
 
 tasks = [Task.make_random(t) for t in range(N)]
 ordered = sorted(tasks)
-selected = greedy_students(M, ordered)
 
+selected = greedy_students(M, ordered)
+selected2 = greedy_students2(M, ordered)
 acum_score = 0
 acum_effort = 0
+print("MEJORADO:")
 for s in selected:
+    acum_score += s.score
+    acum_effort += s.effort
+    print("%s" %(s))
+print("\neffort: %i score: %i"%(acum_effort, acum_score))
+print("-"*30)
+print("NO MEJORADO:")
+acum_score = 0
+acum_effort = 0
+for s in selected2:
     acum_score += s.score
     acum_effort += s.effort
     print("%s" %(s))
