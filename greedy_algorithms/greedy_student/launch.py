bbc02cfe04e46074b041ec43e745d253f9a9a4a7
Kevin Zhang <kazp058@gmail.com>
2023-08-28 22:02:31 -0500

    fix: :ambulance: fixed import error

--- greedy_algorithms/greedy_student/launch.py
+++ greedy_algorithms/greedy_student/launch.py
@@ -1,7 +1,8 @@
-from greedy_algorithms.greedy_student.models import *
+from models import *
 
 M = 60
 N = 100
+
 
 def greedy_students2(grade, tasks):
     selected_tasks = []
@@ -17,12 +18,13 @@ def greedy_students2(grade, tasks):
         prefered_idtask = -1
         for __idtask in range(len(tasks)):
             task = tasks[__idtask]
-            if (__idtask not in selected_tasks 
-                or prefered_idtask < 0 
-                or (tasks[prefered_idtask].effort >= task.effort 
+            if (__idtask not in selected_tasks
+                or prefered_idtask < 0
+                or (tasks[prefered_idtask].effort >= task.effort
                     and tasks[prefered_idtask].score < task.score)):
                 prefered_idtask = __idtask
     return selected_tasks
+
 
 def greedy_students(grade, tasks):
     selected_tasks = []
@@ -31,8 +33,8 @@ def greedy_students(grade, tasks):
         prefered_idtask = None
         for __idtask in range(len(tasks)):
             task = tasks[__idtask]
-            if ((prefered_idtask == None) 
-                or (tasks[prefered_idtask].effort >= task.effort and tasks[prefered_idtask].score <= task.score)):
+            if ((prefered_idtask == None)
+                    or (tasks[prefered_idtask].effort >= task.effort and tasks[prefered_idtask].score <= task.score)):
                 prefered_idtask = __idtask
         task = tasks.pop(prefered_idtask)
         selected_tasks.append(task)
@@ -41,9 +43,10 @@ def greedy_students(grade, tasks):
     return selected_tasks
 
 # Diseñe un algoritmo que seleccione las tareas
-# que le otorgan una nota mayor o igual a la 
+# que le otorgan una nota mayor o igual a la
 # minima que requieren el menor esfuerzo del
 # estudiante
+
 
 tasks = [Task.make_random(t) for t in range(N)]
 ordered = sorted(tasks)
@@ -56,8 +59,8 @@ print("MEJORADO:")
 for s in selected:
     acum_score += s.score
     acum_effort += s.effort
-    print("%s" %(s))
-print("\neffort: %i score: %i"%(acum_effort, acum_score))
+    print("%s" % (s))
+print("\neffort: %i score: %i" % (acum_effort, acum_score))
 print("-"*30)
 print("NO MEJORADO:")
 acum_score = 0
@@ -65,5 +68,5 @@ acum_effort = 0
 for s in selected2:
     acum_score += s.score
     acum_effort += s.effort
-    print("%s" %(s))
-print("\neffort: %i score: %i"%(acum_effort, acum_score))
+    print("%s" % (s))
+print("\neffort: %i score: %i" % (acum_effort, acum_score))
